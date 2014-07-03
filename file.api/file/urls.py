dec1d65f4993aa9acd57707419a48eb7da98a390
Emilio Mariscal <emi420@gmail.com>
2014-07-03 15:42:23 -0300

    Fixes

--- file.api/file/urls.py
+++ file.api/file/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^upload/$', 'file.views.create'),
+    url(r'^create/$', 'file.views.create'),
+    url(r'^createBase64/$', 'file.views.createBase64'),
 )
