f77f3874a5dde4d0f8a215dc40937b4d64ec2b4a
usuario <usuario@linux2.(none)>
2014-09-10 13:37:04 -0300

    parametro classCreation

--- shared.api/decorators.py
+++ shared.api/decorators.py
@@ -82,13 +82,54 @@ class VoolksAPIAuthRequired(object):
                 return response
 
             # Check for class data.api class creation permission
-            elif request.get_full_path().find("/classes/") >= 0 and request.META["REQUEST_METHOD"] == "POST" and responseObj['permissions'].find("-classCreation") >= 0:
-                res = {}
-                res['code'] = 400
-                res['text'] = "API permission denied"
-                response = HttpResponse(json.dumps(res), content_type="application/json")
-                response['Access-Control-Allow-Origin'] = '*'
-                return response
+            elif request.get_full_path().find("/classes/") >= 0 and (request.META["REQUEST_METHOD"] == "PUT" or request.META["REQUEST_METHOD"] == "POST") and responseObj['permissions'].find("-classCreation") >= 0:
+
+                # Get last record added in this class
+                tmp_last_req = requests.get("http://" + request.get_host() + "/classes/" + (request.get_full_path().split('/'))[2] + """?sort={"_id":-1}&limit=1""", headers={'X-Voolks-App-Id': app, 'X-Voolks-Api-Key': key},verify=False)
+
+                # If request for last record added was ok
+                if tmp_last_req.status_code == 200:
+
+                    # Last record added to class
+                    tmp_last =  json.loads(tmp_last_req.text)
+
+                    # Check if request ask for creation of new class
+                    if not tmp_last: # is empty?
+                        res = {}
+                        res['code'] = 400
+                        res['text'] = "API permission denied"
+                        response = HttpResponse(json.dumps(res), content_type="application/json")
+                        response['Access-Control-Allow-Origin'] = '*'
+                        return response
+
+                    # Check if request ask for insert or update of existing class, but with different structure
+                    tmp_data_req = {}
+                    if request.META["REQUEST_METHOD"] == "PUT":
+                        tmp_data_req = request.PUT.items()[0][0]
+                    else: # POST
+                        tmp_data_req = request.POST.items()[0][0]
+                    tmp_data = json.loads(tmp_data_req)
+                    new_fields_requested = False
+                    for key in tmp_data:
+                        if not key in tmp_last['result'][0]:
+                            new_fields_requested = True
+                            break
+                    if new_fields_requested:
+                        res = {}
+                        res['code'] = 400
+                        res['text'] = "API permission denied"
+                        response = HttpResponse(json.dumps(res), content_type="application/json")
+                        response['Access-Control-Allow-Origin'] = '*'
+                        return response
+
+                else:
+                    # Internal error getting last added record
+                    res = {}
+                    res['code'] = 400
+                    res['text'] = "API permission denied * INTERNAL ERROR"
+                    response = HttpResponse(json.dumps(res), content_type="application/json")
+                    response['Access-Control-Allow-Origin'] = '*'
+                    return response
 
             response = self.f(*args)
             response['Access-Control-Allow-Origin'] = responseObj['domain']
